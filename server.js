e5c314b74565e5db58468140ec31d15d598d3aa4
eliza2526 <elizabeth.thomas2@georgebrown.ca>
2025-02-16 23:00:32 -0500

    final commit

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ app.use('/graphql', graphqlHTTP({
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:4000/graphql`);
 });
